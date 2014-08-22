daa1cbeb0e02bd8507fb27f364e0f41c3d261100
Cory Robinson <crr0929@gmail.com>
2014-08-22 12:21:49 -0500

    cleaned up reading in data to be more concise; added a .gitignore file

--- Cleaning/project/run_analysis.R
+++ Cleaning/project/run_analysis.R
@@ -5,18 +5,22 @@ setwd('~/Coursera/Cleaning/project')
 ## first things first: we want to merge the train and test data sets into
 ## one data set.
 
-# read in the training data
-X_train <- read.table("data/UCI HAR Dataset/train/X_train.txt",
-                    sep="", header=FALSE)
-y_train <- read.table("data/UCI HAR Dataset/train/y_train.txt",
-                    sep="", header=FALSE)
-subject_train <- read.table("data/UCI HAR Dataset/train/subject_train.txt",
-                          sep="", header=FALSE)
+# defining filenames to be passed into read.table functions
+data_dir <- "data/UCI\ HAR\ Dataset"
+train_dir <- paste(data_dir, "/train", sep = "")
+test_dir <- paste(data_dir, "/test", sep = "")
+X_tr_filename <- paste(train_dir, "/X_train.txt", sep = "")
+y_tr_filename <- paste(train_dir, "/y_train.txt", sep = "")
+sub_tr_filename <- paste(train_dir, "/subject_train.txt", sep = "")
+X_ts_filename <- paste(test_dir, "/X_test.txt", sep = "")
+y_ts_filename <- paste(test_dir, "/y_test.txt", sep = "")
+sub_ts_filename <- paste(test_dir, "/subject_test.txt", sep = "")
 
-# read in the testing data
-X_test <- read.table("data/UCI HAR Dataset/test/X_test.txt",
-                     sep="", header=FALSE)
-y_test <- read.table("data/UCI HAR Dataset/test/y_test.txt",
-                    sep="", header=FALSE)
-subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt",
-                          sep="", header=FALSE)
+# reading in the data
+X_train <- read.table(X_tr_filename, sep="", header=FALSE)
+y_train <- read.table(y_tr_filename, sep="", header=FALSE)
+subject_train <- read.table(sub_tr_filename, sep="", header=FALSE)
+X_test <- read.table(X_ts_filename, sep="", header=FALSE)
+y_test <- read.table(y_ts_filename, sep="", header=FALSE)
+subject_test <- read.table(sub_ts_filename, sep="", header=FALSE)
+
