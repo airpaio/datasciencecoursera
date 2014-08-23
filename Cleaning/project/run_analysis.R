7494f16a0800242526658d2394973ba40cf7330d
Cory Robinson <crr0929@gmail.com>
2014-08-23 18:37:57 -0500

    merged data and replaced activity IDs with word names

--- Cleaning/project/run_analysis.R
+++ Cleaning/project/run_analysis.R
@@ -15,12 +15,66 @@ sub_tr_filename <- paste(train_dir, "/subject_train.txt", sep = "")
 X_ts_filename <- paste(test_dir, "/X_test.txt", sep = "")
 y_ts_filename <- paste(test_dir, "/y_test.txt", sep = "")
 sub_ts_filename <- paste(test_dir, "/subject_test.txt", sep = "")
+features_filename <- paste(data_dir, "/features.txt", sep = "")
+activity_labels_filename <- paste(data_dir, "/activity_labels.txt", sep = "")
 
 # reading in the data
-X_train <- read.table(X_tr_filename, sep="", header=FALSE)
-y_train <- read.table(y_tr_filename, sep="", header=FALSE)
-subject_train <- read.table(sub_tr_filename, sep="", header=FALSE)
-X_test <- read.table(X_ts_filename, sep="", header=FALSE)
-y_test <- read.table(y_ts_filename, sep="", header=FALSE)
-subject_test <- read.table(sub_ts_filename, sep="", header=FALSE)
+X_train <- read.table(X_tr_filename, sep = "", header=FALSE)
+y_train <- read.table(y_tr_filename, sep = "", header=FALSE)
+subject_train <- read.table(sub_tr_filename, sep = "", header=FALSE)
+X_test <- read.table(X_ts_filename, sep = "", header=FALSE)
+y_test <- read.table(y_ts_filename, sep = "", header=FALSE)
+subject_test <- read.table(sub_ts_filename, sep = "", header=FALSE)
+features <- read.table(features_filename, sep = "", header=FALSE, 
+                       stringsAsFactors=FALSE)
+activity_labels <- read.table(activity_labels_filename, sep = "", header=FALSE)
+
+
+
+## Now lets merge some stuff together using cbind or rbind
+
+# NOTE:
+# dim(X_train) = 7352 561
+# dim(y_train) = 7351 1
+# dim(subject_train) = 7351 1
+# dim(X_test) = 2947 561
+# dim(y_test) = 2947 1
+# dim(subject_test) = 2947 1
+# dim(features) = 561 2
+# dim(activity_labels) = 6 2
+
+# we should bind these together by columns (cbind), first by subject, then
+# by labels (y_) then by the data (X_)
+
+training <- cbind(subject_train, y_train, X_train)
+testing <- cbind(subject_test, y_test, X_test)
+tidy_data <- rbind(training, testing)
+names(tidy_data) <- c( 'SubjectID', 'ActivityID', features$V2)
+
+# get the indices of the mean and std columns
+index_mean_std <- grep("mean|std", features$V2)
+
+# get the mean and std columns of the features data
+features_mean_std <- features[index_mean_std,]
+
+subjects <- rbind(subject_train, subject_test)
+ActivityIDs <- rbind(y_train, y_test)
+tidy_data_mean_std <- cbind(subjects, ActivityIDs, 
+                            tidy_data[,features_mean_std$V2])
+names(tidy_data_mean_std) <- c( 'SubjectID', 'ActivityID', features_mean_std$V2)
+
+
+# replacing Activity ID numbers with word labels
+activityID = 1
+for (label in activity_labels$V2) {
+    tidy_data_mean_std[,2] <- gsub(activityID, 
+                                          label, 
+                                          tidy_data_mean_std[,2])
+    activityID <- activityID + 1
+}
+
+
+# Now rename variables to look nice and clean with good names
+
+
 
